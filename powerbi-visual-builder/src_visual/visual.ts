0d31bb1abccda784b1591b921f739445f4fe9846
Idan Goldfarb <idgoldfa@microsoft.com>
2019-02-13 16:02:34 +0200

    work with several visuals

--- powerbi-visual-builder/src_visual/visual.ts
+++ powerbi-visual-builder/src_visual/visual.ts
@@ -4,6 +4,9 @@
 // The main file for the visual
 
 namespace powerbi.extensibility.visual {
+  const simpleBarChart = '{"specification":{"_id":"5iippmyucb","classID":"chart.rectangle","properties":{"name":"Chart","backgroundColor":null,"backgroundOpacity":1},"mappings":{"marginTop":{"type":"value","value":80}},"glyphs":[{"_id":"80erjnptnzt","classID":"glyph.rectangle","properties":{"name":"Glyph"},"table":"goals by teams","marks":[{"_id":"q23wew7bl7n","classID":"mark.anchor","properties":{"name":"Anchor"},"mappings":{"x":{"type":"parent","parentAttribute":"icx"},"y":{"type":"parent","parentAttribute":"icy"}}},{"_id":"2i1k05stiel","classID":"mark.rect","properties":{"visible":true,"shape":"rectangle","name":"Shape1"},"mappings":{"fill":{"type":"scale","table":"goals by teams","expression":"first(Nation)","valueType":"string","scale":"tbwtw9f0vl"},"strokeWidth":{"type":"value","value":1},"opacity":{"type":"value","value":1},"visible":{"type":"value","value":true},"x1":{"type":"parent","parentAttribute":"ix1"},"y1":{"type":"parent","parentAttribute":"iy1"},"x2":{"type":"parent","parentAttribute":"ix2"},"y2":{"type":"parent","parentAttribute":"iy2"},"height":{"type":"scale","table":"goals by teams","expression":"avg(Goals)","valueType":"number","scale":"y9cmv0l8nk"}}}],"mappings":{},"constraints":[]}],"elements":[{"_id":"6zmj599u3qp","classID":"plot-segment.cartesian","glyph":"80erjnptnzt","table":"goals by teams","filter":null,"mappings":{"x1":{"type":"parent","parentAttribute":"x1"},"y1":{"type":"parent","parentAttribute":"y1"},"x2":{"type":"parent","parentAttribute":"x2"},"y2":{"type":"parent","parentAttribute":"y2"}},"properties":{"name":"PlotSegment1","visible":true,"marginX1":0,"marginY1":0,"marginX2":0,"marginY2":0,"sublayout":{"type":"dodge-x","order":null,"ratioX":0.1,"ratioY":0.1,"align":{"x":"start","y":"start"},"grid":{"direction":"x","xCount":null,"yCount":null}},"xData":{"type":"categorical","expression":"first(Team)","valueType":"string","gapRatio":0.1,"visible":true,"side":"default","style":{"tickColor":{"r":0,"g":0,"b":0},"lineColor":{"r":0,"g":0,"b":0},"fontFamily":"Arial","fontSize":12,"tickSize":5},"categories":["Barcelona","Juve","Liverpol","Macabi Haifa","Man City","Man United","P.S.G","Real Madrid"]}}},{"_id":"sxl5hm9m3d","classID":"mark.text","properties":{"name":"Title","visible":true,"alignment":{"x":"middle","y":"top","xMargin":0,"yMargin":30},"rotation":0},"mappings":{"x":{"type":"parent","parentAttribute":"cx"},"y":{"type":"parent","parentAttribute":"oy2"},"text":{"type":"value","value":"goals by teams"},"fontSize":{"type":"value","value":24},"color":{"type":"value","value":{"r":0,"g":0,"b":0}}}}],"scales":[{"_id":"y9cmv0l8nk","classID":"scale.linear<number,number>","properties":{"name":"Scale1","domainMin":0,"domainMax":150},"mappings":{"rangeMin":{"type":"value","value":0}},"inputType":"number","outputType":"number"},{"_id":"tbwtw9f0vl","classID":"scale.categorical<string,color>","properties":{"name":"Scale2","mapping":{"Spain":{"r":166,"g":206,"b":227},"Israel":{"r":31,"g":120,"b":180},"England":{"r":178,"g":223,"b":138},"Italy":{"r":51,"g":160,"b":44},"Franch":{"r":251,"g":154,"b":153}}},"mappings":{},"inputType":"string","outputType":"color"}],"constraints":[],"resources":[]},"defaultAttributes":{"q23wew7bl7n":{"x":0,"y":0},"2i1k05stiel":{"x1":-4.500000000000001,"y1":-16.685000016277478,"x2":4.500000000000001,"y2":16.685000016277478,"cx":0,"cy":0,"width":9.000000000000002,"height":33.370000032554955,"strokeWidth":0.1,"opacity":0.1},"sxl5hm9m3d":{"x":0,"y":75,"fontSize":6,"opacity":0.25}},"tables":[{"name":"goals by teams","columns":[{"displayName":"Category","name":"Team","type":"string","metadata":{"kind":"categorical","orderMode":"alphabetically"},"powerBIName":"Team"},{"displayName":"Measure","name":"Goals","type":"number","metadata":{"kind":"numerical"},"powerBIName":"Goals"},{"displayName":"Legend","name":"Nation","type":"string","metadata":{"kind":"categorical","orderMode":"alphabetically"},"powerBIName":"Nation"}]}],"inference":[{"objectID":"6zmj599u3qp","dataSource":{"table":"goals by teams"},"axis":{"expression":"first(Team)","type":"categorical","property":"xData"}},{"objectID":"y9cmv0l8nk","scale":{"classID":"scale.linear<number,number>","expressions":["avg(Goals)"],"properties":{"mapping":"mapping"}},"dataSource":{"table":"goals by teams"}},{"objectID":"tbwtw9f0vl","scale":{"classID":"scale.categorical<string,color>","expressions":["first(Nation)"],"properties":{"mapping":"mapping"}},"dataSource":{"table":"goals by teams"}}],"properties":[{"objectID":"sxl5hm9m3d","target":{"attribute":"text"},"type":"text","default":"goals by teams","displayName":"Title/text","powerBIName":"sxl5hm9m3dTitle_text"},{"objectID":"y9cmv0l8nk","target":{"property":"domainMin"},"type":"number","displayName":"Scale1/domainMin","powerBIName":"y9cmv0l8nkScale1_domainMin"},{"objectID":"y9cmv0l8nk","target":{"property":"domainMax"},"type":"number","displayName":"Scale1/domainMax","powerBIName":"y9cmv0l8nkScale1_domainMax"},{"objectID":"5iippmyucb","target":{"attribute":"marginLeft"},"type":"number","default":50,"displayName":"Chart/marginLeft","powerBIName":"5iippmyucbChart_marginLeft"},{"objectID":"5iippmyucb","target":{"attribute":"marginRight"},"type":"number","default":50,"displayName":"Chart/marginRight","powerBIName":"5iippmyucbChart_marginRight"},{"objectID":"5iippmyucb","target":{"attribute":"marginTop"},"type":"number","default":80,"displayName":"Chart/marginTop","powerBIName":"5iippmyucbChart_marginTop"},{"objectID":"5iippmyucb","target":{"attribute":"marginBottom"},"type":"number","default":50,"displayName":"Chart/marginBottom","powerBIName":"5iippmyucbChart_marginBottom"}]}';
+  const simplePieChart = '{"specification":{"_id":"3vcx2sdvqoj","classID":"chart.rectangle","properties":{"name":"Chart","backgroundColor":null,"backgroundOpacity":1},"mappings":{"marginTop":{"type":"value","value":80},"marginRight":{"type":"value","value":100}},"glyphs":[{"_id":"0f9ijim4qhsj","classID":"glyph.rectangle","properties":{"name":"Glyph"},"table":"goals by teams","marks":[{"_id":"rry0dkp9tm","classID":"mark.anchor","properties":{"name":"Anchor"},"mappings":{"x":{"type":"parent","parentAttribute":"icx"},"y":{"type":"parent","parentAttribute":"icy"}}},{"_id":"2u0tv82igtk","classID":"mark.rect","properties":{"visible":true,"shape":"rectangle","name":"Shape1"},"mappings":{"fill":{"type":"scale","table":"goals by teams","expression":"first(Nation)","valueType":"string","scale":"0gsibynypbt"},"strokeWidth":{"type":"value","value":1},"opacity":{"type":"value","value":1},"visible":{"type":"value","value":true},"x1":{"type":"parent","parentAttribute":"ix1"},"y1":{"type":"parent","parentAttribute":"iy1"},"x2":{"type":"parent","parentAttribute":"ix2"},"y2":{"type":"parent","parentAttribute":"iy2"},"height":{"type":"scale","table":"goals by teams","expression":"avg(Goals)","valueType":"number","scale":"cxxix9fnz0q"},"stroke":{"type":"value","value":{"r":0,"g":0,"b":0}}}}],"mappings":{},"constraints":[]}],"elements":[{"_id":"9k1i0oi0xr","classID":"plot-segment.polar","glyph":"0f9ijim4qhsj","table":"goals by teams","filter":null,"mappings":{"x1":{"type":"parent","parentAttribute":"x1"},"x2":{"type":"parent","parentAttribute":"x2"},"y1":{"type":"parent","parentAttribute":"y1"},"y2":{"type":"parent","parentAttribute":"y2"}},"properties":{"name":"PlotSegment1","visible":true,"sublayout":{"type":"dodge-x","order":null,"ratioX":0.1,"ratioY":0.1,"align":{"x":"start","y":"start"},"grid":{"direction":"x","xCount":null,"yCount":null}},"xData":{"type":"categorical","expression":"first(Team)","valueType":"string","gapRatio":0,"visible":true,"side":"default","style":{"tickColor":{"r":0,"g":0,"b":0},"lineColor":{"r":0,"g":0,"b":0},"fontFamily":"Arial","fontSize":12,"tickSize":5},"categories":["Barcelona","Juve","Liverpol","Macabi Haifa","Man City","Man United","P.S.G","Real Madrid"]},"marginX1":0,"marginY1":0,"marginX2":0,"marginY2":0,"startAngle":0,"endAngle":360,"innerRatio":0,"outerRatio":0.9,"curve":[[{"x":-1,"y":0},{"x":-0.25,"y":-0.5},{"x":0.25,"y":0.5},{"x":1,"y":0}]],"normalStart":-0.2,"normalEnd":0.2,"equalizeArea":true}},{"_id":"yvjnlby981","classID":"mark.text","properties":{"name":"Title","visible":true,"alignment":{"x":"middle","y":"top","xMargin":0,"yMargin":30},"rotation":0},"mappings":{"x":{"type":"parent","parentAttribute":"cx"},"y":{"type":"parent","parentAttribute":"oy2"},"text":{"type":"value","value":"goals by teams"},"fontSize":{"type":"value","value":24},"color":{"type":"value","value":{"r":0,"g":0,"b":0}}}},{"_id":"tkqoh0cm6m","classID":"legend.categorical","properties":{"visible":true,"alignX":"end","alignY":"end","fontFamily":"Arial","fontSize":14,"textColor":{"r":0,"g":0,"b":0},"name":"Legend1","scale":"0gsibynypbt"},"mappings":{"x":{"type":"parent","parentAttribute":"x2"},"y":{"type":"parent","parentAttribute":"y2"}}}],"scales":[{"_id":"0gsibynypbt","classID":"scale.categorical<string,color>","properties":{"name":"Scale1","mapping":{"Spain":{"r":166,"g":206,"b":227},"Israel":{"r":31,"g":120,"b":180},"England":{"r":178,"g":223,"b":138},"Italy":{"r":51,"g":160,"b":44},"Franch":{"r":251,"g":154,"b":153}}},"mappings":{},"inputType":"string","outputType":"color"},{"_id":"cxxix9fnz0q","classID":"scale.linear<number,number>","properties":{"name":"Scale2","domainMin":0,"domainMax":150},"mappings":{"rangeMin":{"type":"value","value":0}},"inputType":"number","outputType":"number"}],"constraints":[],"resources":[]},"defaultAttributes":{"rry0dkp9tm":{"x":0,"y":0},"2u0tv82igtk":{"x1":-2.25,"y1":-7.5082500073248655,"x2":2.25,"y2":7.5082500073248655,"cx":0,"cy":0,"width":4.5,"height":15.016500014649731,"strokeWidth":0.1,"opacity":0.1},"yvjnlby981":{"x":-6.25,"y":75,"fontSize":6,"opacity":0.25}},"tables":[{"name":"goals by teams","columns":[{"displayName":"Category","name":"Team","type":"string","metadata":{"kind":"categorical","orderMode":"alphabetically"},"powerBIName":"Team"},{"displayName":"Measure","name":"Goals","type":"number","metadata":{"kind":"numerical"},"powerBIName":"Goals"},{"displayName":"Legend","name":"Nation","type":"string","metadata":{"kind":"categorical","orderMode":"alphabetically"},"powerBIName":"Nation"}]}],"inference":[{"objectID":"9k1i0oi0xr","dataSource":{"table":"goals by teams"},"axis":{"expression":"first(Team)","type":"categorical","property":"xData"}},{"objectID":"0gsibynypbt","scale":{"classID":"scale.categorical<string,color>","expressions":["first(Nation)"],"properties":{"mapping":"mapping"}},"dataSource":{"table":"goals by teams"}},{"objectID":"cxxix9fnz0q","scale":{"classID":"scale.linear<number,number>","expressions":["avg(Goals)"],"properties":{"mapping":"mapping"}},"dataSource":{"table":"goals by teams"}}],"properties":[{"objectID":"yvjnlby981","target":{"attribute":"text"},"type":"text","default":"goals by teams","displayName":"Title/text","powerBIName":"yvjnlby981Title_text"},{"objectID":"cxxix9fnz0q","target":{"property":"domainMin"},"type":"number","displayName":"Scale2/domainMin","powerBIName":"cxxix9fnz0qScale2_domainMin"},{"objectID":"cxxix9fnz0q","target":{"property":"domainMax"},"type":"number","displayName":"Scale2/domainMax","powerBIName":"cxxix9fnz0qScale2_domainMax"},{"objectID":"3vcx2sdvqoj","target":{"attribute":"marginLeft"},"type":"number","default":50,"displayName":"Chart/marginLeft","powerBIName":"3vcx2sdvqojChart_marginLeft"},{"objectID":"3vcx2sdvqoj","target":{"attribute":"marginRight"},"type":"number","default":100,"displayName":"Chart/marginRight","powerBIName":"3vcx2sdvqojChart_marginRight"},{"objectID":"3vcx2sdvqoj","target":{"attribute":"marginTop"},"type":"number","default":80,"displayName":"Chart/marginTop","powerBIName":"3vcx2sdvqojChart_marginTop"},{"objectID":"3vcx2sdvqoj","target":{"attribute":"marginBottom"},"type":"number","default":50,"displayName":"Chart/marginBottom","powerBIName":"3vcx2sdvqojChart_marginBottom"}]}';
+  
   let isInitialized = false;
   const initializeCallbacks = [];
 
@@ -60,12 +63,17 @@ namespace powerbi.extensibility.visual {
     private lastOptions: VisualUpdateOptions;
     private selectionIds: ISelectionId[];
     private currentSelections: ISelectionId[];
+    private firstUpdate: boolean;
+    protected template2: any; // for testing
 
     constructor(options: VisualConstructorOptions) {
       try {
         this.selectionManager = options.host.createSelectionManager();
         this.selectionManager.registerOnSelectCallback((selectionsIds) => {this.onSelectCallBack(selectionsIds)});
-        this.template = "<%= templateData %>" as any;
+        debugger;
+        this.firstUpdate = true;
+
+        this.template2 = "<%= templateData %>" as any;
         this.enableTooltip = "<%= enableTooltip %>" as any;
         this.container = options.element;
         this.divChart = document.createElement("div");
@@ -73,10 +81,6 @@ namespace powerbi.extensibility.visual {
         this.divChart.style.pointerEvents = "none";
         this.host = options.host;
         options.element.appendChild(this.divChart);
-        this.chartTemplate = new CharticulatorContainer.ChartTemplate(
-          this.template
-        );
-        this.properties = this.getProperties(null);
 
         if (this.enableTooltip) {
           // Handles mouse move events for displaying tooltips.
@@ -96,14 +100,14 @@ namespace powerbi.extensibility.visual {
 
     public updateSelections() {
       debugger;
-      //Idan - add interactivity utils
-      let highlights = [];
-      let selecionsID = this.selectionManager.getSelectionIds();
+      // Idan - add interactivity utils
+      const highlights = [];
+      const selecionsID = this.selectionManager.getSelectionIds();
       if (selecionsID && selecionsID[0]){
         for (let i = 0; i < this.selectionIds.length; i++) {
           highlights.push(null);
           for (let j = 0; j < selecionsID.length; j++) {
-            if ((<any>this.selectionIds[i]).includes(selecionsID[j])){
+            if ((this.selectionIds[i] as any).includes(selecionsID[j])){
               highlights[i] = true
             }
           }
@@ -291,6 +295,26 @@ namespace powerbi.extensibility.visual {
 
     public update(options: VisualUpdateOptions) {
       debugger;
+      if (this.firstUpdate){
+        // let number = Math.floor(Math.random() * 2);
+        // if (number == 1){
+        //   this.template = JSON.parse(simpleBarChart);
+        // } else {
+        //   this.template = JSON.parse(simplePieChart);
+        // }
+        // this.chartTemplate = new CharticulatorContainer.ChartTemplate(
+        //   this.template
+        // );
+
+        this.template = this.template2;
+        
+        this.chartTemplate = new CharticulatorContainer.ChartTemplate(
+          this.template
+        );
+        this.properties = this.getProperties(null);
+      }
+      this.firstUpdate = false;
+
       this.lastOptions = options;
       this.highlights = (options as any).highlights;
       if(!this.highlights || this.highlights.length ==0){
@@ -394,7 +418,7 @@ namespace powerbi.extensibility.visual {
             // Idan - use new interface!
 
             // Make selection ids:
-            let selectionIDs: visuals.ISelectionId[] = [];
+            const selectionIDs: visuals.ISelectionId[] = [];
             const selectionID2RowIndex = new WeakMap<ISelectionId, number>();
             dataset.tables[0].rows.forEach((row, i) => {
               const selectionID = (this.host as any)
